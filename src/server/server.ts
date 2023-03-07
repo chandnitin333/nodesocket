f856596e7d40c6a4967fbe1a69b13bb05c02bc5b
Nitin Chandekar <nitin.chandekar@jetsynthesys.com>
2023-03-07 14:17:00 +0530

    first commit

---
+++ src/server/server.ts
@@ -0,0 +1,25 @@
+import http from 'http'
+import socketIO from 'socket.io'
+
+const port: number = 5000
+
+class App {
+    private server: http.Server
+    private port: number
+
+    constructor(port: number) {
+        this.port = port
+
+        this.server = new http.Server()
+        const io = new socketIO.Server(this.server)
+    }
+
+    public Start() {
+        this.server.listen(this.port, () => {
+            console.log(`Server listening on Port ${this.port}.`)
+        })
+
+    }
+}
+
+new App(port).Start()
